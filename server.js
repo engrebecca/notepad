631b138e48493ca31e7121b504d6c5fed531767a
reng <reng412@gmail.com>
2020-10-15 10:02:34 -0700

    Add API routes

--- server.js
+++ server.js
@@ -1,30 +1,26 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
-// =============================================================
-// Will display the notes.html page
-app.get("/notes", function(req, res) {
-    res.sendFile(path.join(__dirname, "public/notes.html"));
-  });
-
-// Will display the index.html page
-app.get("/*", function(req, res) {
-res.sendFile(path.join(__dirname, "public/index.html"));
-});
-
+// ============================================================
 // Will return all saved notes from database
 app.get("/api/notes", function(req, res) {
     fs.readFile(path.join(__dirname, "db/db.json"), "utf8", function(err, data){
@@ -37,14 +33,31 @@ app.get("/api/notes", function(req, res) {
   });
 
 app.post("/api/notes", function(req, res) {
-    let newNote = req.body;
-
+    let newNote = (req.body);
     console.log(newNote);
+    // fs.appendFile(path.join(__dirname, "db/db.json"), newNote, function(err){
+    //     if (err){
+    //         return console.log(err);
+    //     }
+    //     return res.json(newNote);
+    // });
+    readFileAsync(path.join(__dirname, "db/db.json"), "utf8").then(function(data){
+        const notesData = JSON.parse(data);
+        console.log(notesData);
+        notesData.push(newNote);
+        console.log(notesData);
+    })
+});
 
-    fs.writefile(path.join(__dirname, "db/db.json"), newNote);
+// Will display the notes.html page
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
+  });
 
-    res.json(newNote);
-    });
+// Will display the index.html page
+app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public/index.html"));
+});
 
 // Starts the server to begin listening
 // =============================================================
